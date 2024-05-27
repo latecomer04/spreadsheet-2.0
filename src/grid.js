6ad94d6b810f38097924ca0e3148e9dd3fce2515
latecomer04 <latecomer04@gmail.com>
2024-05-28 00:53:06 +0530

    UI-completed

---
+++ src/grid.js
@@ -0,0 +1,48 @@
+let rows = 100;
+let cols = 26;
+
+let addressRowCont = document.querySelector(".address-row-cont");
+let addressColCont = document.querySelector(".address-col-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
+
+
+
+//now we will insert 100 rows number divs in the vertical col below the dummy cell
+for (let i = 0; i < rows; i++) {
+  // now we will create a div that will contain the row number
+  let addressCol = document.createElement("div");
+  addressCol.innerText = i + 1;
+  addressCol.setAttribute("class", "address-col");
+  addressColCont.appendChild(addressCol);
+}
+
+//same for top row to contain the col no
+for (let i = 0; i < cols; i++) {
+  let addressRow = document.createElement("div");
+  addressRow.innerText = String.fromCharCode(65 + i);
+  addressRow.setAttribute("class", "address-row");
+  addressRowCont.appendChild(addressRow);
+}
+
+//make main grid here
+for(let i=0;i<rows;i++){
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class","row-cont");
+    for(let j=0;j<cols;j++){
+        let cell = document.createElement("div");
+        cell.setAttribute("class","cell");
+        cell.setAttribute("contenteditable",true);
+        addListenerForAddressBar(cell,i,j);
+        rowCont.appendChild(cell);
+    }
+    cellsCont.appendChild(rowCont);
+}
+
+function addListenerForAddressBar(cell,i,j){
+    cell.addEventListener("click",(e)=>{
+        let rowId = i+1;
+        let colId = String.fromCharCode(65+j);
+        addressBar.value = `${rowId}${colId}`;
+    })
+}
